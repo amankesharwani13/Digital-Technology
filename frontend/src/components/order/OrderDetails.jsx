ab132b2046123f945209eef4db6debce56ca8bbd
Aman Kesharwani <117504719+amankesharwani13@users.noreply.github.com>
2024-05-14 00:11:23 +0530

    Update OrderDetails.jsx

--- frontend/src/components/order/OrderDetails.jsx
+++ frontend/src/components/order/OrderDetails.jsx
@@ -37,9 +37,12 @@ const OrderDetails = () => {
         <div className="col-12 col-lg-9 mt-5 order-details">
           <div className="d-flex justify-content-between align-items-center">
             <h3 className="mt-5 mb-4">Your Order Details</h3>
-            <a className="btn btn-success" href="/invoice/orders/orders-id">
-              <i className="fa fa-print"></i> Invoice
-            </a>
+            <Link
+              to={`/invoice/order/${order?._id}`}
+              className="btn btn-success ms-2"
+            >
+            <i className="fa fa-print"></i> Invoice
+            </Link>
           </div>
           <table className="table table-striped table-bordered">
             <tbody>
