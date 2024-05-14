a7bf1281663fedd2463a1e5010b8938a1fa062bb
Aman Kesharwani <117504719+amankesharwani13@users.noreply.github.com>
2024-05-14 17:10:46 +0530

    Update seeder.js

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,7 +4,7 @@ import Product from "../models/product.js";
 
 const seedProducts = async()=>{
     try{
-        await mongoose.connect("mongodb://amankesharwani56781:WCjI0WNEpjayFYvc@ac-rhzvafh-shard-00-00.sxmbb0j.mongodb.net:27017,ac-rhzvafh-shard-00-01.sxmbb0j.mongodb.net:27017,ac-rhzvafh-shard-00-02.sxmbb0j.mongodb.net:27017/?ssl=true&replicaSet=atlas-f61pnv-shard-0&authSource=admin&retryWrites=true&w=majority&appName=DigitalTechnology");
+        await mongoose.connect("mongodb+srv://amankesharwani56781:WCjI0WNEpjayFYvc@digitaltechnology.sxmbb0j.mongodb.net/digitalTechnology?retryWrites=true&w=majority&appName=DigitalTechnology");
 
         await Product.deleteMany();
         console.log("Products are deleted");
