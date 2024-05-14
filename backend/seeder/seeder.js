e0af0284d8c37ae5cd5c2f73e99575f84ea60b04
Aman Kesharwani <117504719+amankesharwani13@users.noreply.github.com>
2024-05-14 17:46:23 +0530

    Update seeder.js

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import products from "./data.js";
 import Product from "../models/product.js";
 
+mongoose.set('strictQuery', true); 
 const seedProducts = async()=>{
     try{
         await mongoose.connect("mongodb+srv://amankesharwani56781:WCjI0WNEpjayFYvc@digitaltechnology.sxmbb0j.mongodb.net/digitalTechnology?retryWrites=true&w=majority&appName=DigitalTechnology")
