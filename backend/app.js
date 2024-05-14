2dbf64f528a08844a3bbbfa61719282fdb1ec3f7
Aman Kesharwani <117504719+amankesharwani13@users.noreply.github.com>
2024-05-14 16:21:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,11 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDatabase } from "./config/dbConnect.js";
 import errorMiddleware from "./middlewares/errors.js";
-import cors from "cors";
+
+import path from "path";
+// import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Handle Uncaught exceptions
 process.on("uncaughtException", (err) => {
@@ -13,7 +17,9 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-dotenv.config({ path: "backend/config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config({ path: "backend/config/config.env" });
+}
 
 // Connecting to database
 connectDatabase();
@@ -26,15 +32,6 @@ app.use(
     },
   })
 );
-
-app.use(cors(
-  {
-    origin:["https://deploy-mern-1whq.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
-  }
-));
-
 app.use(cookieParser());
 
 // Import all routes
@@ -42,11 +39,20 @@ import productRoutes from "./routes/products.js";
 import authRoutes from "./routes/auth.js";
 import orderRoutes from "./routes/order.js";
 import paymentRoutes from "./routes/payment.js";
+import { fileURLToPath } from "url";
 
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", orderRoutes);
 app.use("/api/v1", paymentRoutes);
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+}
 
 // Using error middleware
 app.use(errorMiddleware);
