1b56abc41f6a0a5a532a33e34502aa76a1be2cd1
Aman Kesharwani <117504719+amankesharwani13@users.noreply.github.com>
2024-09-03 22:12:46 +0530

    Update seeder.js

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -5,7 +5,7 @@ import Product from "../models/product.js";
 // mongoose.set('strictQuery', true); 
 const seedProducts = async()=>{
     try{
-        await mongoose.connect("mongodb+srv://amankesharwani1309:amankesharwani1309@digital.21id0.mongodb.net/?retryWrites=true&w=majority&appName=Digital")
+        await mongoose.connect("mongodb+srv://amankesharwani1309:amankesharwani1309@digital.21id0.mongodb.net/Digital?retryWrites=true&w=majority&appName=Digital")
 
         await Product.deleteMany();
         console.log("Products are deleted");
